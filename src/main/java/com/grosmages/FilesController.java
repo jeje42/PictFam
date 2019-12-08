79278f79ed54b35623094293829eb5f2bfd9080d
Jérôme Guidon <guidon@live.fr>
2019-12-08 18:51:06 +0100

    Service get image works

--- src/main/java/com/grosmages/FilesController.java
+++ src/main/java/com/grosmages/FilesController.java
@@ -1,7 +1,11 @@
 package com.grosmages;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +26,27 @@ public class FilesController {
 	private PhotoRepository photoRepository;
 	
 	@GetMapping(
-			value = "/thumnails/{id}",
+			value = "/thumnail/{id}",
+			produces = MediaType.IMAGE_JPEG_VALUE
+	)
+	public @ResponseBody byte[] getThumnail(@PathVariable("id") long id) throws IOException {
+		Photo photo = photoRepository.findById(id).orElse(null);		
+		if(photo != null) {
+			return photo.getThumnail();
+		}
+		
+		return null;
+	}
+	
+	@GetMapping(
+			value = "/photo/{id}",
 			produces = MediaType.IMAGE_JPEG_VALUE
 	)
 	public @ResponseBody byte[] getImage(@PathVariable("id") long id) throws IOException {
 		Photo photo = photoRepository.findById(id).orElse(null);		
 		if(photo != null) {
-			return photo.getThumnail();
+			InputStream is = new FileInputStream(new File(photo.getPath()));
+			return IOUtils.toByteArray(is);
 		}
 		
 		return null;
