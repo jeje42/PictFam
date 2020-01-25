06c3ae5ca8992498b5a3adb6bf50764c98be60cd
Jérôme Guidon <guidon@live.fr>
2020-01-25 16:34:39 +0100

    File scan: handle errors if a directory cannot be read

--- src/main/java/com/grosmages/filesscan/FilesUtil.java
+++ src/main/java/com/grosmages/filesscan/FilesUtil.java
@@ -1,31 +1,50 @@
 package com.grosmages.filesscan;
 
 import java.io.IOException;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FilesUtil {
-	public List<Path> findFilesInDirectory(String path, Pattern pattern) {
+	
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
+	public Collection<Path> findFilesInDirectory(String sPath, Pattern pattern) {
+		Collection<Path> toReturn = new ArrayList<>();
+		
 		try {
-			return Files.walk(Paths.get(path))
-			.filter(Files::isRegularFile)
-			.filter(file -> {
-				if (pattern == null) return true;
-				else return pattern.matcher(file.getFileName().toString()).matches();
-			})
-			.collect(Collectors.toList());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return new ArrayList<>();
+			Files.walkFileTree(Paths.get(sPath), new SimpleFileVisitor<Path>() {
+				@Override
+				public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+					if (Files.isRegularFile(file)
+							&& (pattern == null || pattern.matcher(file.getFileName().toString()).matches())) {
+						toReturn.add(file);
+					}
+					return FileVisitResult.CONTINUE;
+				}
+				
+				@Override
+				public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+					log.error("Error reading path: {}", file);
+					return FileVisitResult.SKIP_SUBTREE;
+				}
+			});
+		} catch (IOException e1) {
+			log.error("Error reading subdirectories of {}", sPath);
+			log.error(e1.getMessage());
 		}
+		
+		return toReturn;
 	}
 }
